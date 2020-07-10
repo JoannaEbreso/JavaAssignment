cef09ea72e05ab8b9f2a09f945b4aefddf0e5671
JoannaEbreso <jebreso@yahoo.com>
2020-07-10 04:27:32 +0100

    Java Assignment

---
+++ src/AirlineReservationTest.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class AirlineReservationTest {
+    public static void main(String[] args) {
+        AirlineReservation airline = new AirlineReservation(10);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter 1 for first class, 2 for economy class and any other number to exit: ");
+
+        int classChoice = sc.nextInt();
+
+        while(classChoice==1 || classChoice==2){
+            if(classChoice==1){
+                airline.firstClass();
+            }
+            else{
+                airline.economyClass();
+            }
+            System.out.println("Enter 1 for first class, 2 for economy class and any other number to exit: ");
+            classChoice = sc.nextInt();
+        }
+    }
+}
+
